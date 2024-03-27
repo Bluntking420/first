af6161c2d1a34fd2c456b84a20227895e1bae618
Apoorv <149949178+ApoorvGoswami@users.noreply.github.com>
2024-03-27 22:27:32 +0530

    Update : Rename fps level scene name

--- Assets/script/MouseLook.cs
+++ Assets/script/MouseLook.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class MouseLook : MonoBehaviour
 {
-    public float mouseSensitivity=100f;
+    public float mouseSensitivity = 100f;
     public Transform playerBody;
-    float xRotation = 0f;
+    float xRotation;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,11 +16,13 @@ public class MouseLook : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float mouseX = Input.GetAxis("Mouse X")*mouseSensitivity*Time.deltaTime;
-        float mouseY = Input.GetAxis("Mouse Y")*mouseSensitivity*Time.deltaTime;
-        xRotation-=mouseY;
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+
         xRotation = Mathf.Clamp(xRotation, -90f, 90);
-        transform.localRotation = Quaternion.Euler(xRotation,0,0);
+        transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
         playerBody.Rotate(Vector3.up * mouseX);
     }
 }
