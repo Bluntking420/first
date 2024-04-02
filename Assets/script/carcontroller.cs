8d9b219adba7ae0e1719ceb5dc9d56af5fc60c46
Bluntking420 <82205848+Bluntking420@users.noreply.github.com>
2024-04-02 17:06:15 +0530

    car fix

--- Assets/script/carcontroller.cs
+++ Assets/script/carcontroller.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class CarController : MonoBehaviour
 {
@@ -15,6 +16,24 @@ public class CarController : MonoBehaviour
     public float slipAngle;
     public float Speed;
     public AnimationCurve steeringCurve;
+    public float MaxSpeed;
+    private float speedClamped;
+    public int isEngineRunning;
+    public float RPM;
+    public float RedLight;
+    public float idleRPM;
+    public TMP_Text rpmText;
+    public TMP_Text gearText;
+    public Transform rpmNeedle;
+    public int currentGear;
+    public float minNeedleRotation;
+    public float maxNeedleRotation;
+    public float[] gearRatios;
+    public float differentialRatio;
+    private float currentTorque;
+    private float clutch;
+    private float wheelRPM;
+    public AnimationCurve hpToRPMCurve;
     // Start is called before the first frame update
     void Start()
     {
@@ -24,7 +43,11 @@ public class CarController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Speed= PlayerRB.velocity.magnitude;
+        rpmNeedle.rotation = Quaternion.Euler(0, 0, Mathf.Lerp(minNeedleRotation, maxNeedleRotation, RPM / RedLight));
+        rpmText.text = RPM.ToString("0,000") + "RPM";
+        gearText.text = (currentGear + 1).ToString();
+        Speed = colliders.RRWheel.rpm*colliders.RRWheel.radius*2f*Mathf.PI/10;
+        speedClamped = Mathf.Lerp(speedClamped, Speed, Time.deltaTime);
         CheckInput();
         ApplyMotor();
         ApplyWheelPosition(); 
@@ -32,11 +55,21 @@ public class CarController : MonoBehaviour
         ApplyBrake();
     }
 
+
+
     void CheckInput()
     {
         gasInput = Input.GetAxis("Vertical");
+        if (Mathf.Abs(gasInput) > 0 && isEngineRunning==0)
+        {
+           StartCoroutine( GetComponent<EngineAudio>().StartEngine());
+        }
+        
+        
+        
         steeringInput = Input.GetAxis("Horizontal");
         slipAngle = Vector3.Angle(transform.forward,PlayerRB.velocity-transform.forward);
+        clutch = (Input.GetKey(KeyCode.LeftShift) ? 0 : Mathf.Lerp(clutch, 1, Time.deltaTime));
         if (slipAngle < 120)
         {
             if (gasInput < 0)
@@ -75,15 +108,33 @@ public class CarController : MonoBehaviour
 
       
     }
-    
-    
+
+
     void ApplyMotor()
     {
-        colliders.RRWheel.motorTorque = motorPower * gasInput;
-        colliders.RLWheel.motorTorque = motorPower * gasInput;
-       
+        currentTorque = CalculateTorque();
+                colliders.RRWheel.motorTorque=currentTorque*gasInput ;
+                colliders.RLWheel.motorTorque=currentTorque*gasInput ;
+            
     }
-
+    float CalculateTorque()
+    {
+        float torque = 0;
+        if(isEngineRunning > 0)
+        {
+            if (clutch < 0.1f)
+            {
+                RPM = Mathf.Lerp(RPM, Mathf.Max(idleRPM, RedLight * gasInput) + Random.Range(-50, 50), Time.deltaTime);
+            }
+            else
+            {
+                wheelRPM = Mathf.Abs((colliders.RRWheel.rpm + colliders.RLWheel.rpm) / 2F) * gearRatios[currentGear] * differentialRatio;
+                RPM=Mathf.Lerp(RPM, Mathf.Max(idleRPM-100,wheelRPM), Time.deltaTime*3f);
+                torque=(hpToRPMCurve.Evaluate(RPM/RedLight)*motorPower/RPM)*gearRatios[currentGear]*differentialRatio*5252f*clutch;
+            }
+        }
+         return torque;
+    }
 
     void ApplyWheelPosition()
     {
@@ -101,6 +152,14 @@ public class CarController : MonoBehaviour
         wheelMesh.transform.position = position;
         wheelMesh.transform.rotation = quat;
     }
+    public float GetSpeedRatio()
+    { 
+     var gas=Mathf.Clamp(Mathf.Abs(gasInput),0.5f,1f);
+        return speedClamped * gas / MaxSpeed;
+    
+    }
+
+
 
 
    [System.Serializable]
