c899a1aedebffcddb5ef3a9f5f2498c14d075c35
Bluntking420 <82205848+Bluntking420@users.noreply.github.com>
2024-04-16 09:31:08 +0530

    16thapril

--- Assets/script/Ui/MainMenu.cs
+++ Assets/script/Ui/MainMenu.cs
@@ -5,31 +5,16 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    public string playSceneName = "1"; // Name of the scene to load when "Play" is clicked
-    public GameObject settingsPanel; // Reference to the settings panel GameObject in the scene
+  
 
-    public void Play()
+    public void PlayGame()
     {
-        SceneManager.LoadScene(playSceneName); // Load the play scene
+        SceneManager.LoadSceneAsync(1); // Load the play scene
                                                // Function to load a scene by its name
      
 
     }
 
-    public void OpenSettings()
-    {
-        settingsPanel.SetActive(true); // Activate the settings panel
-    }
-
-    public void CloseSettings()
-    {
-        settingsPanel.SetActive(false); // Deactivate the settings panel
-    }
-       // Function to load a scene by its name
-    public void LoadScene(string sceneName)
-    {
-        SceneManager.LoadScene(sceneName);
-    }
 
     public void Quit()
     {
